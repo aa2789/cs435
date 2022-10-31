3122c7f156a828fdd7e7d676efd082ee9d83812d
abhiarun <aa2789@njit.edu>
2022-10-31 17:15:12 -0400

    changes to hw3

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -7,7 +7,8 @@ void radixSort(int r, int c, char** P, char** S){
     
     for(int j=c-1;j>=0;j--){
         int counts[27]={0};
-        unordered_map<int,char*> map;
+        char* map[r];
+      
         for(int i=0;i<r;i++){
             char c = P[i][j];
             if(isspace(c)){
@@ -32,17 +33,19 @@ void radixSort(int r, int c, char** P, char** S){
             counts[(c-'A')+1]-=1;
             }
           
-            map[index]=P[i];
+            map[index] = P[i];
          }
          for(int i=0;i<r;i++){
-            P[i]=map[i];
+            P[i] = map[i];
+
          }
 
     }
 }
-int populateSArray(char** S){
+
+int populateSArray(char** S, string fileName){
     ifstream infile;
-    infile.open("f.txt");
+    infile.open(fileName);
     int index=0;
     if(infile.is_open()){
         string str;
@@ -63,6 +66,20 @@ int populateSArray(char** S){
     return index;
 }
 
+void writeToOutputFile(char** P, int r, string outputFile){
+    ofstream fout;
+    fout.open(outputFile);
+    for(int i=0;i<r;i++){
+        string str="";
+        for(int j=0;j<21;j++){
+            if(isspace(P[i][j]))break;
+            str+=P[i][j];
+        }
+        fout<<str<<endl;
+    }
+    fout.close();
+}
+
 void initializePArray(char** P, char** S, int r){
     for(int i=0;i<r;i++){
         P[i] = S[i];
@@ -70,25 +87,27 @@ void initializePArray(char** P, char** S, int r){
 }
 
 int main(){
+    string infile="";
+    string outfile="";
+    cout<<"Please specify the input file (default = f.txt): ";
+    getline(cin,infile);
+    cout<<"Please specify the output file (default = g.txt): ";
+    getline(cin, outfile);
+    if(infile.length()==0)infile="f.txt";
+    if(outfile.length()==0)outfile="g.txt";
 
     char** S = new char*[1000];
     for(int i=0;i<1000;i++){
         S[i]=new char[21];
     }
-    int numWords=populateSArray(S);
-
-
+    int numWords=populateSArray(S,infile);
     char* P[numWords];
+
     initializePArray(P,S,numWords);
 
     radixSort(numWords,21,P,S);
-    for(int i=0;i<numWords;i++){
-        for(int j=0;j<21;j++){
-            if(isspace(P[i][j]))break;
-            cout<<P[i][j];
-        }
-        cout<<endl;
-    }
+
+    writeToOutputFile(P,numWords,outfile);
 
     for(int i=0;i<1000;i++){
         delete[] S[i];
